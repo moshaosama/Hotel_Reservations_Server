6147a272570cb0f21722d53b558fa172f4367855
moshaosama <mohamedOSFekry@gmail.com>
2025-07-01 17:35:48 +0300

    modify: updated app.module.ts with new module imports or configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HotelsModule } from './hotels/hotels.module';
+import { hotel } from './hotels/entities/hotel.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      username: 'root',
+      password: '',
+      database: 'hotel_booking',
+      entities: [hotel],
+    }),
+    HotelsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
