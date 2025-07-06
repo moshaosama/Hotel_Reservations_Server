8a01f9eda7bbd598789504c27b1a463c6cd784a1
moshaosama <mohamedOSFekry@gmail.com>
2025-07-06 20:04:08 +0300

    modify: registered history module in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,25 +6,20 @@ import { HotelsModule } from './hotels/hotels.module';
 import { hotel } from './hotels/entities/hotel.entity';
 import { HistoryModule } from './history/history.module';
 import { history } from './history/entities/history.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'mysql',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USER'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_DATABASE'),
-        entities: [hotel, history],
-      }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      username: 'root',
+      password: '',
+      database: 'hotel_booking',
+      entities: [hotel, history],
     }),
     HotelsModule,
     HistoryModule,
